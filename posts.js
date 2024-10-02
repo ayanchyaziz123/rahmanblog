edbd5aa503e49eb4edbf29fe264a526637be5d1b
ayanchyaziz123 <aaiz9642@gmail.com>
2024-10-02 13:58:51 -0400

    added

--- posts.js
+++ posts.js
@@ -88,7 +88,7 @@ const posts = [
         category: 'Dynamic Programming', 
         contentPath: 'blog_content/blog8/blog8.html',
         summary: 'You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?',
-        image: 'blog_content/blog8/blog8_image.jpg'
+        image: 'blog_content/blog8/blog8_image.png'
     },
     { 
         id: 9, 
